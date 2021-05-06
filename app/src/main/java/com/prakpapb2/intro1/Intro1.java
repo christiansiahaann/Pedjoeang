622a691e725bd0a2d43d997c2102b4a40ceb2205
RRSiahaan <ridhosiahaan2@gmail.com>
2021-05-06 13:09:32 +0700

    Nambah font

---
+++ app/src/main/java/com/prakpapb2/intro1/Intro1.java
@@ -0,0 +1,14 @@
+package com.prakpapb2.intro1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Intro1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro2);
+    }
+}
