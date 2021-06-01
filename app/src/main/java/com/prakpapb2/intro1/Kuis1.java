533df076fa07bfedbc1386e96d49eb77539b4b56
risa06 <risamahardika89@gmail.com>
2021-06-01 11:23:21 +0700

    Menambahkan Intro1-3, MulaiKuis, Kuis1-5, Ulasan, Widget

---
+++ app/src/main/java/com/prakpapb2/intro1/Kuis1.java
@@ -0,0 +1,14 @@
+package com.prakpapb2.intro1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kuis1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuis1);
+    }
+}
